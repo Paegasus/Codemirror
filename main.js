b944c88c85c739868f60f0b9189646f99aa7ed96
lahcenebelbachir@gmail.com <lahcenebelbachir@gmail.com>
2021-06-24 05:47:31 +0100

    EditorView test

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 var basicSetup = require("@codemirror/basic-setup");
 var langJS = require("@codemirror/lang-javascript");
 
+let editor = new EditorView({
+  state: EditorState.create({
+    extensions: [basicSetup, javascript()]
+  }),
+  parent: document.body
+})
