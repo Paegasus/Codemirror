7b70c664a5dd3c8fba6aee4436990430b1465724
lahcenebelbachir@gmail.com <lahcenebelbachir@gmail.com>
2021-06-24 16:40:16 +0100

    Theme fix

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 import {EditorState, EditorView, basicSetup} from "@codemirror/basic-setup"
 import {javascript} from "@codemirror/lang-javascript"
-import { oneDarkTheme } from "@codemirror/theme-one-dark";
+import {oneDarkTheme} from "@codemirror/theme-one-dark";
+import {oneDark} from "@codemirror/theme-one-dark";
 
-let editor = new EditorView({
-  state: EditorState.create({
-    extensions: [basicSetup, javascript(), oneDarkTheme]
+let editor = new EditorView
+({
+  state: EditorState.create
+  ({
+    extensions:
+    [
+      basicSetup,
+      javascript(),
+      oneDark
+    ]
   }),
   parent: document.body
 })
