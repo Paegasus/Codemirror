002698522d9bac8a6c4b73f8a0261f9bc2e6f84d
Lahcéne Belbachir <lahcenebelbachir@gmail.com>
2022-01-01 18:47:15 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import {EditorState, EditorView, basicSetup} from "@codemirror/basic-setup"
 import {javascript} from "@codemirror/lang-javascript"
-import {oneDarkTheme} from "@codemirror/theme-one-dark";
 import {oneDark} from "@codemirror/theme-one-dark";
 
 let editor = new EditorView
