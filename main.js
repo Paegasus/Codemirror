72adf52f69855cf90ac8dddf8883c4929748f827
Lahcéne Belbachir <lahcenebelbachir@gmail.com>
2022-01-04 23:58:19 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,16 +2,18 @@ import {EditorState, EditorView, basicSetup} from "@codemirror/basic-setup"
 import {javascript} from "@codemirror/lang-javascript"
 import {oneDark} from "@codemirror/theme-one-dark";
 
+let editorState = EditorState.create
+({
+  extensions:
+  [
+    basicSetup,
+    javascript(),
+    oneDark
+  ]
+});
+
 let editor = new EditorView
 ({
-  state: EditorState.create
-  ({
-    extensions:
-    [
-      basicSetup,
-      javascript(),
-      oneDark
-    ]
-  }),
+  state: editorState,
   parent: document.body
 })
